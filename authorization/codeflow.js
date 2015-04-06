1838bcfaed55c724ed7435c1538191936168fb9f
Pat Sabpisal <ssabpisa@me.com>
2015-04-06 18:27:00 -0400

    Moving on

--- authorization/codeflow.js
+++ authorization/codeflow.js
@@ -17,8 +17,13 @@ var utils = require('../utils.js');
 var phantomUtils = require('../phantom-utils.js')
 var config = require('../config.js').authorization;
 var testOptions = require('../config.js').options;
+
+var KEY_ACCESS_TOKEN = "AccessToken"
+
 //Allow self signed certs
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+
 
 var state = {
     test : {}
@@ -100,7 +105,7 @@ describe('get access token in code flow process', function(){
              var aurl = config.uri + '/auth';
              aurl += '/' + '?' + utils.getQueryString(parameters);
 
-            this.timeout(15000);
+            this.timeout(5000);
             phantom.create(function (ph) {
                 _ph = ph;
                 ph.createPage(function (page) {
@@ -137,7 +142,7 @@ describe('get access token in code flow process', function(){
    });
 
    it('presents scope form', function(done){
-         this.timeout(15000);
+         this.timeout(5000);
         _page.render("screen1.png");
         _page.set('onUrlChanged', function(url) {
             //TODO: seems hacky and unreliable
@@ -162,169 +167,15 @@ describe('get access token in code flow process', function(){
 
         }, function(result){
              setTimeout(function(){
-                _page.render("screen2.png");
+                _ph.exit();
              }, 2000);
         }, config.automation.shift());
    });
 
 });
 
-  		// describe('Login with provided credential' , function(){
-    //             var RQID = 'x';
-    //             // console.log(state.test);
-  		// // 	it('should reject bad credential', function(done){
-	  	// // 		var postdata = {};
-	  	// // 		//construct the post data from config
-	  	// // 		for(var i in state.test.login.fields){
-    // //                 if (state.test.login.fields.hasOwnProperty(i)) {
-    // // 	  				var name = state.test.login.fields[i];
-    // // 	  				postdata[name] = 'x' + config.loginFieldsValue[name];
-    // //                 }
-	  	// // 		}
 
-	  	// // 		state.stateVariable = 'xyz';  //TODO: gen rand string
-
-	  	// // 		//perform a login
-				// // request
-				// // 	.post(utils.getRelativePath(config.uri,
-    // //                     state.test.login['action']))
-				// //     .type('form')
-				// // 	.set('User-Agent',testOptions.userAgentValue)
-				// //     .redirects(0)
-				// // 	.send(postdata)
-				// // 	.end(function(err, res){
-				// // 		should.not.exist(err, res.text);
-				// // 		//TODO: check that it rejects (somehow?)
-				// // 		done();
-				// // 	});
-
-
-	  	// // 	});
-
-  		// 	it('should accept correct credential', function(done){
-	  	// 		var postdata = {};
-	  	// 		//construct the post data from config
-	  	// 		// for(var i in state.test.login.fields){
-    //   //               if (state.test.login.fields.hasOwnProperty(i)) {
-	  	// 		// 	   var name = state.test.login.fields[i];
-	  	// 		// 	   postdata[name] = config.loginFieldsValue[name];
-    //   //               }
-	  	// 		// }
-    //             //'d3a25599-f8fb-409f-b36e-e5de1dd7c96f'
-
-    //             postdata = {
-    //                 'GrowerUserName': 'TestGrower',
-    //                 'GrowerPassword': 'Valmont#1',
-    //                 'BaseStationUrl': 'https://test.basestation3.com',
-    //                 'RequestId': RQID
-    //             };
-    //             console.log(postdata)
-
-	  	// 		state.stateVariable = 'xyz';  //TODO: gen rand string
-
-	  	// 		//perform a login
-
-				// request
-				// 	.post('/OADA/AuthenticateGrower')
-				//     .type('form')
-				// 	.set('User-Agent',
-    //                     testOptions.userAgentValue)
-				//     .redirects(0)
-				// 	.send(postdata)
-				// 	.end(function(err, res){
-    //                     console.log("responded with: ")
-    //                     console.log(res.text)
-				// 		should.not.exist(err, res.text);
-				// 		done();
-				// 	});
-
-
-	  	// 	});
-
-	  	// 	it('redirect to login', function(done){
-    //             state.stateVariable = '4OQ03SK5USSxKVTRlIAh1Ew';
-	  	// 		var parameters = {
-				// 	    	'response_type' : 'code',
-				// 	    	'client_id': config.goldClient['client_id'],
-				// 	    	'state' : state.stateVariable,
-				// 	    	'redirect_uri': config.goldClient['redirect_uri'],
-				// 	    	'scope': 'bookmarks.machines',
-    //                         'prompt': 'consent'
-				// };
-
-			 //    var aurl = '/auth';
-    //             aurl += '/' + '?' + utils.getQueryString(parameters);
-
-
-			 //    var req = request
-			 //    		.get(aurl)
-			 //    		.set('User-Agent',testOptions.userAgentValue)
-			 //    		.type('form');
-
-			 //    req.expect(302)
-			 //        .end(function(err,res){
-			 //    		should.not.exist(err, res);
-    //                     state.test.loginURL = res.header.location;
-			 //    		done();
-			 //    	});
-	  	// 	});
-
-
-
-//   describe('Grant Screen', function(){
-//   		var $;
-//   		before(function(){
-//   			$ = cheerio.load(state.test.grantscreen.html);
-//   		});
-
-//   		it('should correctly displays all requested scopes', function(done){
-
-//   			//How should we decide whether all scopes are displayed
-//   			done();
-//   		});
-
-//   		it('should receive access code from server', function(done){
-//   			//steal access code from query string
-
-//   			var data = {};
-
-//   			$('form input[type=text],' +
-//                 'input[type=password],' +
-//                 'input[type=hidden],' +
-//                 ' textarea').each(function(){
-// 				data[$(this).attr('name')] = $(this).attr('value');
-// 			});
-
-// 			var formAction = $('form').attr('action');
-// 			var postURL = utils.getRelativePath(config.uri, formAction);
-
-// 			request
-// 				.post(postURL)
-// 				.set('User-Agent',testOptions.userAgentValue)
-// 				.type('form')
-// 				.send(data)
-// 				.redirects(0)
-// 				.expect(302)
-// 				.end(function(err, res){
-// 					should.not.exist(err, res.text);
-// 					//intercept the redirection
-// 					var intercepted = utils.
-//                                       getQueryParameters(res.headers.location);
-// 					intercepted.should.have.property('code');
-// 					state.test['access_code'] = intercepted.code;
-// 					//make sure states are equal
-// 					assert.equal(state.stateVariable, intercepted.state);
-
-// 					done();
-// 				});
-
-
-//   		});
-
-//   });
-
-
-  describe('Exchanging Access Token Step', function(){
+describe('Exchanging Access Token Step', function(){
   		var cert;
   		var secret;
   		var tokenEndpoint;
@@ -340,6 +191,28 @@ describe('get access token in code flow process', function(){
 				config.goldClient['key_id']
 			);
   		});
+
+        it('should reject bad clientID', function(done){
+            var parameters = {
+                    'grant_type': 'authorization_code',
+                    'code': state.test['access_code'],
+                    'redirect_uri': config.goldClient['redirect_uri'],
+                    'client_id': "hello@world.com",
+                    'client_secret': secret
+              };
+
+              var postURL = utils.getRelativePath(config.uri, tokenEndpoint);
+
+              request
+              .post(postURL)
+              .type('form')
+              .set('User-Agent', testOptions.userAgentValue)
+              .send(parameters)
+              .end(function(err, res){
+                JSON.parse(res.text).should.not.have.property(KEY_ACCESS_TOKEN)
+                done();
+              });
+        });
 
         it('should succeed', function(done){
               var parameters = {
@@ -361,7 +234,33 @@ describe('get access token in code flow process', function(){
               .end(function(err, res){
                   should.not.exist(err, res.text);
                   state.test.tokenResponse = JSON.parse(res.text);
-                  console.log(res.text)
+                  state.test.tokenResponse.should.have.property(KEY_ACCESS_TOKEN);
+                  // console.log("ACTOKEN: " + state.test.tokenResponse[KEY_ACCESS_TOKEN])
+                  done();
+              });
+
+        });
+
+
+        it('should fail when reusing access code', function(done){
+              var parameters = {
+                    'grant_type': 'authorization_code',
+                    'code': state.test['access_code'],
+                    'redirect_uri': config.goldClient['redirect_uri'],
+                    'client_id': config.goldClient['client_id'],
+                    'client_secret': secret
+              };
+
+              var postURL = utils.getRelativePath(config.uri, tokenEndpoint);
+
+              request
+              .post(postURL)
+              .type('form')
+              .set('User-Agent', testOptions.userAgentValue)
+              .send(parameters)
+              .end(function(err, res){
+                  var tryJSON = JSON.parse(res.text);
+                  tryJSON.should.not.have.property(KEY_ACCESS_TOKEN)
                   done();
               });
 
@@ -430,28 +329,6 @@ describe('get access token in code flow process', function(){
 
 //   		});
 
-//         it('should reject bad clientID', function(done){
-//             var parameters = {
-//                 'grant_type': 'authorization_code',
-//                 'code': state.test['access_code'],
-//                 'redirect_uri': config.goldClient['redirect_uri'],
-//                 'client_id': 'bad@example.com',
-//                 'client_secret': secret
-//             };
-
-//             var postURL = utils.getRelativePath(config.uri, tokenEndpoint);
-
-//             request
-//                 .post(postURL)
-//                 .type('form')
-//                 .set('User-Agent', testOptions.userAgentValue)
-//                 .send(parameters)
-//                 .expect(200)
-//                 .end(function(err, res){
-//                     should.exist(err, res.text);
-//                     done();
-//                 });
-//         });
 
 //   		it('should exchange access code for a token', function(done){
 // 			var parameters = {
